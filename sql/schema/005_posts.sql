7e13eb09a84d11a52a620cde487fa9cb6110d999
shadywarder <shadywarder@gmail.com>
2025-02-16 22:15:31 +0300

    feat: write db schemas

---
+++ sql/schema/005_posts.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+CREATE TABLE posts (
+  id UUID UNIQUE NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  title TEXT NOT NULL,
+  url TEXT NOT NULL UNIQUE,
+  description TEXT NOT NULL,
+  published_at TIMESTAMP NOT NULL,
+  feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE posts;
